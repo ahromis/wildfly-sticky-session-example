9c3e0345d705340fe4b71a29e4f756ac795b1b14
Alexis Hassler <alexis@sewatech.org>
2014-06-29 17:28:19 +0200

    Change test to read Cookie instead of servlet output

--- src/test/java/fr/sewatech/wildfly/SessionIdTest.java
+++ src/test/java/fr/sewatech/wildfly/SessionIdTest.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -41,16 +44,29 @@ public class SessionIdTest {
 
             URL servlet = new URL(root, "session");
             HttpURLConnection con = (HttpURLConnection) servlet.openConnection();
+            CookieManager cookieManager = new CookieManager();
+            CookieHandler.setDefault(cookieManager);
+
             con.setRequestMethod("GET");
             int responseCode = con.getResponseCode();
             if (responseCode == 200) {
-                assertThat(readBody(con), endsWith(".xxx"));
+                assertThat(getSessionId(con), endsWith(".xxx"));
             } else {
                 fail("Response code : " + responseCode);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getSessionId(HttpURLConnection con) {
+        List<String> cookies = con.getHeaderFields().get("Set-Cookie");
+        for (String cookie : cookies) {
+            if (cookie.startsWith("JSESSIONID"))
+            return cookie.split(";")[0].split("=")[1];
+        }
+        return null;
     }
 
     private String readBody(HttpURLConnection con) throws IOException {
